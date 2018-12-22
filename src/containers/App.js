78d34ceb922eb148ed3197ed91437d933941e4eb
Dorian Lazar <dorianlazar81@gmail.com>
2018-12-22 18:17:50 +0200

    fixing something

--- src/containers/App.js
+++ src/containers/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   }
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/Websites/BurgerBuilder">
         <Layout>
           <Switch>
             <Route path="/" exact component={BurgerBuilder} />
